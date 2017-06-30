85bd75b493f27f5f34da99222abe16ca18a6dff7
Luo <ar-jun.luo@rakuten.com>
2017-06-30 07:38:01 +0100

    add changes to allow customizing messages

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-import logging
+import os 
+import sys
+import json
+
+import requests
 from flask import Flask, request
 
 
 app = Flask(__name__)
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+# process user input and customize your message here, by default it echoes whatever you say to it
+def process_text(sender_id, message_text):
+	send_message(sender_id, message_text)
+
 
 @app.route('/', methods=['GET'])
 def verify_fb():
@@ -10,14 +43,41 @@ def verify_fb():
     	if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
     		return "Verification token mismatch", 403
     	return request.args["hub.challenge"], 200
-    return "This is great :)", 200
+    return "This is great :))))", 200
 
 @app.route('/', methods=['POST'])
 def webhook():
-        return "ok", 200
+
+    # endpoint for processing incoming messaging events
+    data = request.get_json()
+    if data["object"] == "page":
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    # customize your reply to the user
+                    process_text(sender_id, message_text)
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
 
 @app.errorhandler(500)
 def server_error(e):
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
